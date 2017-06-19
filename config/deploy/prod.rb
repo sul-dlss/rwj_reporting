05c5bdce6ce77ad6e139c49b73b39c96bf730236
Jessie Keck <jessie.keck@gmail.com>
2017-06-19 11:27:51 -0700

    Update capistrano per dept. conventions.

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,8 +1,5 @@
-set :home_directory, "/home/#{fetch(:user)}"
-set :deploy_to, "#{fetch(:home_directory)}/#{fetch(:application)}"
-
 # The only production server at the moment. A second will be added after the node is pupgraded
-server 'libstream.stanford.edu', user: fetch(:user), roles: %w(app)
+server 'libstream.stanford.edu', user: 'media', roles: %w(app)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
