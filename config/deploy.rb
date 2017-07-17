6f6a18589982765f92cd7d01fc77244352379776
Chris Beer <chris@cbeer.info>
2017-07-17 13:02:47 -0700

    Add honeybadger

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :log_level, :info
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
+set :linked_files, fetch(:linked_files, []).push('config/settings.yml', 'config/honeybadger.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
@@ -30,3 +30,5 @@ namespace :deploy do
     end
   end
 end
+
+set :honeybadger_env, fetch(:stage)
